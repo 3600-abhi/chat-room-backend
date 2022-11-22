dcf11a37abd57b27ca004265ebfc3d423067f928
3600-abhi <abhishekjaiswal3600@gmail.com>
2022-11-22 18:02:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const server = app.listen(5000, () => {
+  console.log('listening on *:5000');
+});
+
+const io = require('socket.io')(server);
+
+
+io.on('connection', (socket) => {
+  const name = socket.handshake.query.name
+  socket.on('message', (data) => {
+    const message = {
+      message: data.message,
+      senderName: name,
+      sentAt: Date.now()
+    }
+    io.emit('message', message)
+
+  })
+});
+
